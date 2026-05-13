b4f42a1c7a79bbc7d9c48333bbb4dd7e27b286f8
maher <maher.ahmed@engaz.ai>
2026-05-13 16:59:54 +0300

    feat: initialize project architecture, styles, mocks, and environment configs
    - Set up core folder structure with 'core', 'features', and 'shared' modules.
    - Implemented 7-1 Sass architecture in 'src/styles/' and updated main style entry.
    - Added mock data for accounts, customers, and transactions in 'public/mock/'.
    - Configured 'dev', 'staging', and 'prod' environments in 'angular.json' with file replacements.
    - Created environment files for production, staging, and local development.
    - Updated 'app.config.ts' and 'app.html' for initial application setup.
    - Configured assets to properly serve mock data from the public directory.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,8 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+// Prime NG
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
@@ -7,6 +10,11 @@ import { provideClientHydration, withEventReplay } from '@angular/platform-brows
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes), provideClientHydration(withEventReplay()),
+    providePrimeNG({
+            theme: {
+                preset: Aura
+            }
+        })
   ]
 };
